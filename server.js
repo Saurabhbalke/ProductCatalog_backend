45842e47a337c3fc44aa5db6251a977e7354de1a
saurabh Balke <86031203+Saurabhbalke@users.noreply.github.com>
2025-03-15 23:45:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(cors());
 
 app.use("/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
